93bfd1466f1bc62af9a631f1e499d64138687ae1
Shavaine Brown <35738478+shavaine@users.noreply.github.com>
2023-10-16 00:54:18 -0400

    Practice: Add ForLoop Practice

---
+++ HomeworkForLoops/Program.cs
@@ -0,0 +1,10 @@
+﻿/* Ask the user for a comma-separted list of first names (no spaces). Split the string into a string array
+ * Loop through the array and print "Hello <name> to the console for each person */
+
+Console.WriteLine("Please enter a list of first names (seperated by comma, and no space):");
+List<string> firstNames = Console.ReadLine().Split(',').ToList();
+
+for (int i = 0; i < firstNames.Count; i++)
+{
+    Console.WriteLine($"Hello {firstNames[i]}");
+}
