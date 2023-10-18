68df688ea72a84ea4915da1af95c9f9ddf17b0d4
shavaine <35738478+shavaine@users.noreply.github.com>
2023-10-18 18:01:11 -0400

    Project: Add Guest Book Mini Project

---
+++ MiniProjectGuestList/Party.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MiniProjectGuestList
+{
+    public static class Party
+    {
+        public static (string name, int people) GetNameAndAmount()
+        {
+            Console.Write("What is your name: ");
+            string name = Console.ReadLine();
+            bool validPeople = false;
+            int people;
+
+            do
+            {
+                Console.Write("How many people are in your party: ");
+                validPeople = int.TryParse(Console.ReadLine(), out people);
+
+                if (validPeople == false)
+                {
+                    Console.WriteLine("The value you entered for People is invalid.");
+                } else if (people < 0)
+                {
+                    Console.WriteLine("The amount of people can't be a negative number");
+                    validPeople = false;
+                }
+            } while (validPeople == false);
+
+            return (name, people);
+            
+        }
+
+        public static bool CheckForGuest()
+        {
+            bool output = false; // return false by default
+            bool validAnswer = false;
+            do
+            {
+                Console.Write("Are there anymore guests (Type 'yes', or 'no'): ");
+                string optionAnswer = Console.ReadLine();
+                if (optionAnswer.ToLower() == "yes")
+                {
+                    validAnswer = true;
+                    output = true;
+                } else if (optionAnswer.ToLower() == "no") {
+                    validAnswer = true;
+                    output = false;
+                } else if (optionAnswer != "yes" && optionAnswer != "no")
+                {
+                    validAnswer = false;
+                }
+            } while (validAnswer == false);
+
+            return output;
+        } 
+
+        public static void PrintGuestList(Dictionary<string, int> guestList)
+        {
+            Console.WriteLine(); // Empty Space
+
+            Console.WriteLine($"Guest List: ");
+            foreach (string name in guestList.Keys)
+            {
+                Console.WriteLine($"{name} ");
+            }
+
+            Console.WriteLine(); // Empty Space
+
+            Console.WriteLine($"Total Number of Guest: {guestList.Values.Sum()}");
+        }
+
+    }
+}
---
+++ MiniProjectGuestList/Program.cs
@@ -0,0 +1,26 @@
+﻿using MiniProjectGuestList;
+
+/* Build a Console Guest Book. Ask the user for their name and how many are in their party. 
+ * Keep track of how many people are at the party. 
+ * At the end, print out the guest list and the total number of guest.
+ */
+
+Dictionary<string, int> guestList = new Dictionary<string, int>();
+bool running = true;
+
+do
+{
+    Console.WriteLine("Welcome to the Party!");
+
+    // Gets the name and amount of people in guest's party
+    (string name, int people) = Party.GetNameAndAmount();
+
+    // Added the guest and people amount to Guest List
+    guestList.Add(name, people);
+
+    // Asks if there are anymore Guest and exit loop if there aren't any
+    running = Party.CheckForGuest();
+
+} while (running);
+
+Party.PrintGuestList(guestList);
