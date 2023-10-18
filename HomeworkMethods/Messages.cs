70b1e1829743c7204f8bac09600e25e30cb3198b
shavaine <35738478+shavaine@users.noreply.github.com>
2023-10-18 14:54:12 -0400

    Practice: Add Methods Practice

---
+++ HomeworkMethods/Messages.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeworkMethods
+{
+    public static class Messages
+    {
+        public static void Welcome()
+        {
+            Console.WriteLine("Welcome to the application.");
+        }
+
+        public static string GetUsername() {
+
+            Console.Write("What is your username: ");
+            string username = Console.ReadLine();
+            return username;
+
+        }
+
+        public static void GreetUser(string username)
+        {
+            Console.WriteLine($"Hello {username}");
+        }
+    }
+}
---
+++ HomeworkMethods/Program.cs
@@ -0,0 +1,9 @@
+﻿using HomeworkMethods;
+
+/* Create a welcome method, a method to ask for the user's name, and another to tell that user "Hello <name>".
+ * Call these methods from Program.cs.*/
+
+Messages.Welcome();
+string username = Messages.GetUsername();
+
+Messages.GreetUser(username);
