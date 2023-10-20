a9b44831f75a5848791c9e421999bda63134c709
shavaine <35738478+shavaine@users.noreply.github.com>
2023-10-20 16:47:25 -0400

    Lesson: Add Advance Exception Handling Lesson

---
+++ AdvancedExceptions/Program.cs
@@ -0,0 +1,37 @@
+﻿using static AdvancedExceptions.ErrorMethods;
+
+
+string name = "";
+try
+{
+    DifferentMethod();
+    Console.Write("What is your name: ");
+    name = Console.ReadLine();
+    ComplexMethod(name);
+    SimpleMethod();
+}
+catch (InvalidOperationException ex)
+{
+    Console.WriteLine("You should not be calling these methods.");
+    Console.WriteLine(ex.Message);
+}
+catch (NotImplementedException)
+{
+    Console.WriteLine("You forgot to finish your code!!!");
+}
+catch (Exception ex) when (name.ToLower() == "tim")
+{
+    Console.WriteLine("You used Tim's name, didn't you?");
+}
+catch (Exception ex)
+{
+    Console.WriteLine(ex);
+    /*Console.WriteLine(ex.Message);*/
+}
+finally
+{
+    Console.WriteLine("I always run");
+}
+
+
+Console.ReadLine();
