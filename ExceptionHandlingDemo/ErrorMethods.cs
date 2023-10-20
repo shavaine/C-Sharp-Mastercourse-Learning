2c8de0078f798ff9d388fa4e121ca019f04f14ca
shavaine <35738478+shavaine@users.noreply.github.com>
2023-10-20 16:46:57 -0400

    Lesson: Add Exception Handling Lesson

---
+++ ExceptionHandlingDemo/ErrorMethods.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExceptionHandlingDemo
+{
+    public class ErrorMethods
+    {
+        public static void BadCall()
+        {
+            int[] ages = new int[] { 1, 3, 5, };
+
+            for (int i = 0; i <= ages.Length; i++)
+            {
+                try
+                {
+                    Console.WriteLine(ages[i]);
+                }
+                catch (Exception ex)
+                {
+
+                    Console.WriteLine("We had an error");
+                    Console.WriteLine(ex.Message);
+                    throw new Exception("There was an error handling our array", ex);
+                }
+
+            }
+        }
+    }
+}
---
+++ ExceptionHandlingDemo/Program.cs
@@ -0,0 +1,13 @@
+﻿using ExceptionHandlingDemo;
+
+try
+{
+	ErrorMethods.BadCall();
+}
+catch (Exception ex)
+{
+
+    Console.WriteLine("There was an exception thrown in our app");
+    Console.WriteLine(ex.Message);
+}
+Console.ReadLine();
