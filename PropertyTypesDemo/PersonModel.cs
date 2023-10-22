74762e514c3718f15e821400244e16bb18517155
Shavaine Brown <35738478+shavaine@users.noreply.github.com>
2023-10-22 03:45:30 -0400

    Lesson: Add Property Types Lesson

---
+++ PropertyTypesDemo/PersonModel.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PropertyTypesDemo
+{
+    public class PersonModel
+    {
+        public string FirstName { private get; set; }
+        public string LastName { get; private set; }
+        //public int Age { get; set; }
+
+        private string _password;
+
+        public string Password
+        {
+            set { _password = value; }
+        }
+
+        public string FullName
+        {
+            get 
+            {
+                return $"{FirstName} {LastName}";
+            }
+            
+        }
+
+        private int _age;
+
+        public int Age
+        {
+            get 
+            { 
+                return _age; 
+            }
+            set 
+            { 
+                if (value >= 0 && value < 125)
+                {
+                    _age = value;
+                }
+                else
+                {
+                    throw new ArgumentOutOfRangeException("value", "Age needs to be in a valid range.");
+                }
+            }
+        }
+
+        // public string SSN { get; set; } // Social Security Number 123-45-6789
+        private string _ssn;
+
+        public string SSN
+        {
+            get 
+            {
+                // 123-45-6789
+                string output = "***-**-" + _ssn.Substring(_ssn.Length - 4);
+                return output; 
+            }
+            set { _ssn = value; }
+        }
+
+        public PersonModel()
+        {
+            
+        }
+
+        public PersonModel(string lastName)
+        {
+            LastName = lastName;
+        }
+    }
+}
---
+++ PropertyTypesDemo/Program.cs
@@ -0,0 +1,14 @@
+﻿
+using PropertyTypesDemo;
+
+PersonModel person = new PersonModel("Brown");
+
+person.FirstName = "Shavaine";
+//person.LastName = "Brown";
+person.Age = 27;
+person.SSN = "123-45-6789";
+
+Console.WriteLine(person.FullName);
+Console.WriteLine(person.SSN);
+
+Console.ReadLine();
