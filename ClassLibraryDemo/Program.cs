f33ff1a3fa64a97ad17e5d9cfc6e9db164a43462
Shavaine Brown <35738478+shavaine@users.noreply.github.com>
2023-10-22 23:44:10 -0400

    Lesson: Add Class Library Lesson

---
+++ ClassLibraryDemo/Program.cs
@@ -0,0 +1,16 @@
+﻿using DemoLibrary.Models;
+using System;
+
+namespace ClassLibraryDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            PersonModel person = new PersonModel();
+
+
+            Console.ReadLine();
+        }
+    }
+}
