6cf73f7df369e57c29f8bae451c0af9a6a0ba2eb
Shavaine Brown <35738478+shavaine@users.noreply.github.com>
2023-10-22 22:44:13 -0400

    Lesson: Add Namespace Lesson

---
+++ NamespaceDemo/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using FoundationInfo;
+using NamespaceDemo.Models;
+
+namespace NamespaceDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<PersonModel> people = new List<PersonModel>();
+            PersonModel person = new PersonModel();
+
+            Calculations.Add(4,5);
+
+            Console.ReadLine();
+        }
+    }
+}
