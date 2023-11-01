76c643d32b7d5260016908cd17186b888991fae4
shavaine <35738478+shavaine@users.noreply.github.com>
2023-10-31 23:34:09 -0400

    Practice: Add Extension Methods Mini Project
    Also includes method overloading.

---
+++ Practice/MiniProjectExtensionMethods/PersonModel.cs
@@ -0,0 +1,11 @@
+﻿namespace MiniProjectExtensionMethods
+{
+    public class PersonModel
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; set; }
+        public int NumberOfChildren { get; set; }
+
+    }
+}
---
+++ Practice/MiniProjectExtensionMethods/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace MiniProjectExtensionMethods
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            PersonModel person = new PersonModel();
+
+            person.FirstName = "What is your first name: ".RequestString();
+
+            person.LastName = "What is your last name: ".RequestString();
+
+            person.Age = "What is your age: ".RequestInt(0, 120);
+
+        }
+    }
+}
