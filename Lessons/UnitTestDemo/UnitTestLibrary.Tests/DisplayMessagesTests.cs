d59d6dc134d5bc1eb47e52e8e5faa77d8581ac18
shavaine <35738478+shavaine@users.noreply.github.com>
2023-11-02 00:36:48 -0400

    Lesson: Add Unit Test Lesson

---
+++ Lessons/UnitTestDemo/UnitTestLibrary.Tests/DisplayMessagesTests.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+// Arrange, Act, Assert
+// expected and actual
+namespace UnitTestLibrary.Tests
+{
+    public class DisplayMessagesTests
+    {
+        [Fact]
+        public void GreetingShouldReturnGoodMorningMessage()
+        {
+            // Arrange
+            DisplayMessages messages = new DisplayMessages();
+            string expected = "Good morning Tim";
+
+            // Act
+            string actual = messages.Greeting("Tim", 2);
+
+            // Assert
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void GreetingShouldReturnGoodAfternoonMessage()
+        {
+            // Arrange
+            DisplayMessages messages = new DisplayMessages();
+            string expected = "Good afternoon Tim";
+
+            // Act
+            string actual = messages.Greeting("Tim", 14);
+
+            // Assert
+            Assert.Equal(expected, actual);
+        }
+
+        [Theory]
+        [InlineData("Tim", 0, "Good morning Tim")]
+        public void GreetingShouldReturnExpectedValue(
+            string firstName,
+            int hourOfDay,
+            string expected)
+        {
+            // Arrange
+            DisplayMessages messages = new DisplayMessages();
+            
+
+            // Act
+            string actual = messages.Greeting(firstName, hourOfDay);
+
+            // Assert
+            Assert.Equal(expected, actual);
+        }
+    }
+}
---
+++ Lessons/UnitTestDemo/UnitTestLibrary/DisplayMessages.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// Red, Green, Refactor
+
+namespace UnitTestLibrary
+{
+    public class DisplayMessages
+    {
+        public string Greeting(string firstName, int hourOfTheDay)
+        {
+            string output = "";
+
+            if (hourOfTheDay < 12)
+            {
+                output = $"Good morning {firstName}";
+            } else if (hourOfTheDay < 18)
+            {
+                output = $"Good afternoon {firstName}";
+            }
+            else
+            {
+                output = $"Good evening {firstName}";
+            }
+
+            return output ;
+        }
+    }
+}
