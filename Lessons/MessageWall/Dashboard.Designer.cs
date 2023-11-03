3f6d3c6473455f4b0f03a722fa758f042f243752
shavaine <35738478+shavaine@users.noreply.github.com>
2023-11-03 07:37:51 -0400

    Lesson: Add WinForms Lesson

---
+++ Lessons/MessageWall/Dashboard.Designer.cs
@@ -0,0 +1,111 @@
+﻿namespace MessageWall
+{
+    partial class Dashboard
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.messageLabel = new System.Windows.Forms.Label();
+            this.messageText = new System.Windows.Forms.TextBox();
+            this.messageListBox = new System.Windows.Forms.ListBox();
+            this.messageListBoxLabel = new System.Windows.Forms.Label();
+            this.addMessage = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // messageLabel
+            // 
+            this.messageLabel.AutoSize = true;
+            this.messageLabel.Location = new System.Drawing.Point(61, 39);
+            this.messageLabel.Name = "messageLabel";
+            this.messageLabel.Size = new System.Drawing.Size(112, 29);
+            this.messageLabel.TabIndex = 0;
+            this.messageLabel.Text = "Message";
+            // 
+            // messageText
+            // 
+            this.messageText.Location = new System.Drawing.Point(179, 36);
+            this.messageText.Name = "messageText";
+            this.messageText.Size = new System.Drawing.Size(370, 35);
+            this.messageText.TabIndex = 1;
+            // 
+            // messageListBox
+            // 
+            this.messageListBox.FormattingEnabled = true;
+            this.messageListBox.ItemHeight = 29;
+            this.messageListBox.Location = new System.Drawing.Point(66, 159);
+            this.messageListBox.Name = "messageListBox";
+            this.messageListBox.Size = new System.Drawing.Size(483, 294);
+            this.messageListBox.TabIndex = 3;
+            // 
+            // messageListBoxLabel
+            // 
+            this.messageListBoxLabel.AutoSize = true;
+            this.messageListBoxLabel.Location = new System.Drawing.Point(61, 127);
+            this.messageListBoxLabel.Name = "messageListBoxLabel";
+            this.messageListBoxLabel.Size = new System.Drawing.Size(124, 29);
+            this.messageListBoxLabel.TabIndex = 0;
+            this.messageListBoxLabel.Text = "Messages";
+            // 
+            // addMessage
+            // 
+            this.addMessage.Location = new System.Drawing.Point(555, 36);
+            this.addMessage.Name = "addMessage";
+            this.addMessage.Size = new System.Drawing.Size(79, 35);
+            this.addMessage.TabIndex = 2;
+            this.addMessage.Text = "Add";
+            this.addMessage.UseVisualStyleBackColor = true;
+            this.addMessage.Click += new System.EventHandler(this.addMessage_Click);
+            // 
+            // Dashboard
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(14F, 29F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(681, 517);
+            this.Controls.Add(this.addMessage);
+            this.Controls.Add(this.messageListBox);
+            this.Controls.Add(this.messageText);
+            this.Controls.Add(this.messageListBoxLabel);
+            this.Controls.Add(this.messageLabel);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(7, 7, 7, 7);
+            this.Name = "Dashboard";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label messageLabel;
+        private System.Windows.Forms.TextBox messageText;
+        private System.Windows.Forms.ListBox messageListBox;
+        private System.Windows.Forms.Label messageListBoxLabel;
+        private System.Windows.Forms.Button addMessage;
+    }
+}
+
---
+++ Lessons/MessageWall/Dashboard.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MessageWall
+{
+    public partial class Dashboard : Form
+    {
+        BindingList<string> messages = new BindingList<string>();
+        public Dashboard()
+        {
+            InitializeComponent();
+
+            WireUpList();
+        }
+
+        private void WireUpList()
+        {
+            messageListBox.DataSource = messages;
+            // messageListBox.DisplayMember = nameof(Dashboard.Text); // If you used a model and want to display specific prop
+        }
+
+        private void addMessage_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(messageText.Text))
+            {
+                MessageBox.Show("Please enter a message before trying to add it to the list.",
+                    "Blank Message Field", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            else
+            {
+                messages.Add(messageText.Text);
+                messageText.Text = string.Empty;
+            }
+            messageText.Focus();
+        }
+    }
+}
