84781463f3469de21fed7bb69715e67a867b49b6
shavaine <35738478+shavaine@users.noreply.github.com>
2023-11-03 07:54:47 -0400

    Practice: Add WinForms Homework

---
+++ Practice/HomeworkWinforms/Form1.Designer.cs
@@ -0,0 +1,112 @@
+﻿namespace HomeworkWinforms
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.firstNameLabel = new System.Windows.Forms.Label();
+            this.lastNameText = new System.Windows.Forms.TextBox();
+            this.lastNameLabel = new System.Windows.Forms.Label();
+            this.firstNameText = new System.Windows.Forms.TextBox();
+            this.sayHiButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // firstNameLabel
+            // 
+            this.firstNameLabel.AutoSize = true;
+            this.firstNameLabel.Location = new System.Drawing.Point(187, 110);
+            this.firstNameLabel.Margin = new System.Windows.Forms.Padding(7, 0, 7, 0);
+            this.firstNameLabel.Name = "firstNameLabel";
+            this.firstNameLabel.Size = new System.Drawing.Size(131, 29);
+            this.firstNameLabel.TabIndex = 0;
+            this.firstNameLabel.Text = "First Name";
+            // 
+            // lastNameText
+            // 
+            this.lastNameText.Location = new System.Drawing.Point(334, 189);
+            this.lastNameText.Margin = new System.Windows.Forms.Padding(7, 7, 7, 7);
+            this.lastNameText.Name = "lastNameText";
+            this.lastNameText.Size = new System.Drawing.Size(228, 35);
+            this.lastNameText.TabIndex = 1;
+            // 
+            // lastNameLabel
+            // 
+            this.lastNameLabel.AutoSize = true;
+            this.lastNameLabel.Location = new System.Drawing.Point(192, 195);
+            this.lastNameLabel.Margin = new System.Windows.Forms.Padding(7, 0, 7, 0);
+            this.lastNameLabel.Name = "lastNameLabel";
+            this.lastNameLabel.Size = new System.Drawing.Size(128, 29);
+            this.lastNameLabel.TabIndex = 0;
+            this.lastNameLabel.Text = "Last Name";
+            // 
+            // firstNameText
+            // 
+            this.firstNameText.Location = new System.Drawing.Point(332, 107);
+            this.firstNameText.Margin = new System.Windows.Forms.Padding(7);
+            this.firstNameText.Name = "firstNameText";
+            this.firstNameText.Size = new System.Drawing.Size(228, 35);
+            this.firstNameText.TabIndex = 1;
+            // 
+            // sayHiButton
+            // 
+            this.sayHiButton.Location = new System.Drawing.Point(459, 283);
+            this.sayHiButton.Name = "sayHiButton";
+            this.sayHiButton.Size = new System.Drawing.Size(100, 40);
+            this.sayHiButton.TabIndex = 2;
+            this.sayHiButton.Text = "Say Hi!";
+            this.sayHiButton.UseVisualStyleBackColor = true;
+            this.sayHiButton.Click += new System.EventHandler(this.SayHiButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(14F, 29F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(838, 576);
+            this.Controls.Add(this.sayHiButton);
+            this.Controls.Add(this.firstNameText);
+            this.Controls.Add(this.lastNameText);
+            this.Controls.Add(this.lastNameLabel);
+            this.Controls.Add(this.firstNameLabel);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(7, 7, 7, 7);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label firstNameLabel;
+        private System.Windows.Forms.TextBox lastNameText;
+        private System.Windows.Forms.Label lastNameLabel;
+        private System.Windows.Forms.TextBox firstNameText;
+        private System.Windows.Forms.Button sayHiButton;
+    }
+}
+
---
+++ Practice/HomeworkWinforms/Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HomeworkWinforms
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void SayHiButton_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(firstNameText.Text) && string.IsNullOrEmpty(lastNameText.Text))
+            {
+                MessageBox.Show("Please enter first and last name.", "No Input", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                firstNameText.Focus();
+            } 
+            else if (string.IsNullOrEmpty(firstNameText.Text))
+            {
+                MessageBox.Show("Please enter first name.", "No First Name", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                firstNameText.Focus();
+            }
+            else if (string.IsNullOrEmpty(lastNameText.Text))
+            {
+                MessageBox.Show("Please enter last name.", "No Last Name", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                lastNameText.Focus();
+            } 
+            else
+            {
+                MessageBox.Show($"Hi {firstNameText.Text} {lastNameText.Text}");
+                firstNameText.Text = string.Empty;
+                lastNameText.Text = string.Empty;
+            }
+            
+        }
+    }
+}
