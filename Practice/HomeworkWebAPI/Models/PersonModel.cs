034c00ac62657f7f7e7cceb59c2bb7189414a15e
shavaine <35738478+shavaine@users.noreply.github.com>
2023-11-08 22:33:25 -0500

    Practice: Add ASP.NET Web API Homework

---
+++ Practice/HomeworkWebAPI/Models/PersonModel.cs
@@ -0,0 +1,9 @@
+﻿namespace HomeworkWebAPI.Models
+{
+    public class PersonModel
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public bool IsActive { get; set; }
+    }
+}
