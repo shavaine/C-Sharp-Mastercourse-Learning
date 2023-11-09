93df623934934f18d4127e54033fdb569bca5731
shavaine <35738478+shavaine@users.noreply.github.com>
2023-11-09 16:46:16 -0500

    Lesson: Add Blazor Server Lesson

---
+++ Lessons/BlazorServerMessageWall/Models/MessageModel.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BlazorServerMessageWall.Models;
+
+public class MessageModel
+{
+    [Required]
+    [StringLength(10, MinimumLength = 5)]
+    public string Message { get; set; }
+}
