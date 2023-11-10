4842538b3c797a3a54833b6a6a298501f34b4d76
shavaine <35738478+shavaine@users.noreply.github.com>
2023-11-09 22:41:35 -0500

    Practice: Add Blazor Server Homework

---
+++ Practice/HomeworkBlazorServer/Models/AddressModel.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace HomeworkBlazorServer.Models
+{
+    public class AddressModel
+    {
+        [Required]
+        public string? Address { get; set; }
+
+        [Required]
+        public string? City { get; set; }
+
+        [Required]
+        public string? State { get; set; }
+
+        [Required]
+        [DisplayName("Postal Code")]
+        public string PostalCode { get; set; }
+    }
+}
---
+++ Practice/HomeworkBlazorServer/Models/PersonModel.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace HomeworkBlazorServer.Models
+{
+    public class PersonModel
+    {
+        [Required]
+        [DisplayName("First Name")]
+        public string? FirstName { get; set; }
+
+        [Required]
+        [DisplayName("Last Name")]
+        public string? LastName { get; set; }
+
+        [Required]
+        [DisplayName("Is Active")]
+        public bool IsActive { get; set; }
+    }
+}
