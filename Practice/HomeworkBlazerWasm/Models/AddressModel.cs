7a94a1cf1a986377c4bd19da356829c6eaeae3ae
shavaine <35738478+shavaine@users.noreply.github.com>
2023-11-10 01:36:29 -0500

    Practice: Add Blazor Wasm Homework

---
+++ Practice/HomeworkBlazerWasm/Models/AddressModel.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace HomeworkBlazerWasm.Models
+{
+    public class AddressModel
+    {
+        [Required]
+        public string Address { get; set; }
+
+        [Required]
+        public string City { get; set; }
+
+        [Required]
+        public string Region { get; set; }
+
+        [Required]
+        public string PostalCode { get; set; }
+    }
+}
---
+++ Practice/HomeworkBlazerWasm/Models/PersonModel.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace HomeworkBlazerWasm.Models
+{
+    public class PersonModel
+    {
+        [Required]
+        public string FirstName { get; set; }
+
+        [Required]
+        public string LastName { get; set; }
+
+        [Required]
+        public bool IsActive { get; set; }
+    }
+}
